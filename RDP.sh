34a516bbdeb13b95ab0b125d8e94e1265f084ec1
Fauzan Rifki Maulana <75284956+FRM-Developer@users.noreply.github.com>
2021-02-26 22:15:17 +0700

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,7 +1,7 @@
-sudo useradd -m ALOK
-sudo adduser ALOK sudo
+sudo useradd -m FRM
+sudo adduser FRM sudo
 sudo passwd
-printf "Terminal issue solve - https://youtu.be/XZxxaNWJxT0 \n" >&2
+printf "halah \n" >&2
 {
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
@@ -12,10 +12,9 @@ wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 apt update
 apt -y install anydesk
-sudo adduser ALOK chrome-remote-desktop
+sudo adduser FRM chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nhttps://remotedesktop.google.com/headless \n'
-read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
+su - FRM -c """DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AY0e-g5PwCSygyAkMSgR3Y0f5vOSm9Ya-rBO4YFPUy9J7S3cf8gvxTCd-dbpq8TruAK5DA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)"""
